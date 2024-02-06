054f49cfc6c77bf008ad21f6cf0e25ac2fb89220
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-02-06 10:27:22 +0530

    learn gsap to

---
+++ day1/script.js
@@ -0,0 +1,9 @@
+// initial to final using to in gsap
+// first give element , second object and give css in this object
+gsap.to(".div1", {
+  x: 1000,
+  duration: 3,
+  rotate:360,
+  delay:1,
+  backgroundColor : "red"
+});
