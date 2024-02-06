d03a34e835a535cb4152bd9857ca675fdc12e143
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-02-06 12:21:45 +0530

    this is my first animation projec

--- day1/script.js
+++ day1/script.js
@@ -7,12 +7,55 @@
 //   delay:1,
 //   backgroundColor : "red"
 // });
-// it's final to initial position to come 
+// it's final to initial position to come
 
-gsap.from(".div1", {
-  x: 1000,
-  duration: 3,
-  rotate:360,
-  delay:1,
-  backgroundColor : "red"
+// gsap.from(".div1", {
+//   x: 1000,
+//   duration: 3,
+//   rotate:360,
+//   delay:1,
+//   backgroundColor : "red"
+// });
+// var timeline = gsap.timeline();
+// timeline.to("#box1", {
+//   x: 1200,
+//   duration: 3,
+//   rotate: 360,
+//   delay: 1,
+//   //   backgroundColor : "red"
+//   scale: 0.5,
+// });
+// timeline.to("#box2", {
+//   x: 1200,
+//   duration: 3,
+//   rotate: 360,
+//   // delay: 1,
+//   //   backgroundColor : "red"
+//   scale: 0.5,
+// });
+// timeline.to("#box3", {
+//   x: 1200,
+//   duration: 3,
+//   rotate: 360,
+//   // delay: 1,
+//   //   backgroundColor : "red"
+//   scale: 0.5,
+// });
+var tl = gsap.timeline();
+tl.from("#nav h3", {
+  y: -50,
+  opacity: 0,
+  duration: 0.5,
+  delay: 0.3,
+  stagger: 0.3,
+});
+tl.from("#main h1", {
+  x: -500,
+  opacity: 0,
+  stagger: 0.3,
+});
+tl.from("img", {
+  x: -200,
+  opacity: 0,
+  stagger: 0.3,
 });
